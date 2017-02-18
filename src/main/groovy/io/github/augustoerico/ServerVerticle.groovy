69fce03411ee9086011e36cc14af766f716a0bb6
Erico Silva <augusto.ericosilva@gmail.com>
2017-02-18 10:27:16 -0200

    Add router to server

--- src/main/groovy/io/github/augustoerico/ServerVerticle.groovy
+++ src/main/groovy/io/github/augustoerico/ServerVerticle.groovy
@@ -3,13 +3,26 @@ package io.github.augustoerico
 import io.github.augustoerico.config.Env
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.Route
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
 class ServerVerticle extends AbstractVerticle {
 
     @Override
     void start(Future future) {
+        Router router = Router.router(vertx)
+
+        // TODO move this to somewhere else
+        router.route(HttpMethod.GET, '/').handler { RoutingContext context ->
+            def response = context.response()
+            response.putHeader('content-type', 'text/html')
+                    .end('<h1>Hello, world! Welcome to vertx!</h1>')
+        }
+
         vertx.createHttpServer()
-                .requestHandler { req -> req.response().end('Hello, world') }
+                .requestHandler(router.&accept)
                 .listen(Env.port(), Env.address(), handleResult.curry(future))
     }
 
